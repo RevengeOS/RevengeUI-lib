3313ca4c6a89c4bbbeee7bef62c3f6edab477abf
Lucchetto <lucchetto.tie@live.com>
2020-12-28 13:10:45 +0100

    BitmapUtils: add function to calculate bitmap brightness and saturation
        * From https://github.com/asukim1022/BitmapBrightnessValueApp
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- revengeui/src/main/java/com/revengeos/revengeui/utils/BitmapUtils.java
+++ revengeui/src/main/java/com/revengeos/revengeui/utils/BitmapUtils.java
@@ -3,8 +3,46 @@ package com.revengeos.revengeui.utils;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 
 public class BitmapUtils {
+
+    // return an array with {brightness, saturation}
+    public static float[] calculateBrightnessEstimate(Bitmap bitmap, int pixelSpacing) {
+        int R = 0;
+        int G = 0;
+        int B = 0;
+        int T = 0;
+        float S = 0;
+        int height = bitmap.getHeight();
+        int width = bitmap.getWidth();
+        int n = 0;
+        int[] pixels = new int[width * height];
+        bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+        for (int i = 0; i < pixels.length; i += pixelSpacing * 15) {
+            int color = pixels[i];
+            float[] hsv = new float[3];
+            R += Color.red(color);
+            G += Color.green(color);
+            B += Color.blue(color);
+            T += getColorToBrightness(color);
+            Color.colorToHSV(color, hsv);
+            S += hsv[1];
+            n++;
+        }
+
+        //T = (R + B + G);
+        float[] rtn = {(T / n), (S / n)};
+        return rtn;
+    }
+
+    public static int getColorToBrightness(int color) {
+        int R = Color.red(color);
+        int G = Color.green(color);
+        int B = Color.blue(color);
+        return (int) Math.sqrt(R * R * .241 + G * G * .691 + B * B * .068);
+    }
+
     public static int calculateInSampleSize(
             BitmapFactory.Options options, int reqWidth, int reqHeight) {
         // Raw height and width of image
